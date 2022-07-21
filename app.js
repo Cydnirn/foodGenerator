9b25156bb1a754db8bfe4f0fe1f1e96358a6f854
Cydnirn <senoaji0003@gmail.com>
2022-07-21 13:36:22 +0700

    modified:   app.js new file:   foodMoodPredict.ipynb new file:   public/frontpage.html

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 80
-var http = require('http');
-http.createServer();
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const server = http.createServer(app);
+server.listen(80);
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
