1de5681da70c0386461e6c8935f88a2617aa8096
Cydnirn <senoaji0003@gmail.com>
2022-08-18 00:32:53 +0700

    Delete model dir

--- model/change.py
+++
@@ -1,7 +0,0 @@
-import os
-with open('test.csv', 'r') as file:
-    filedata = file.read()
-    filedata = filedata.replace(';', ',')
-
-    with open('test.csv', 'w') as file:
-        file.write(filedata)
